c7f037dcd6b1e1719aea2943e407987e936a83d5
nathan-zucker <114187880+nathan-zucker@users.noreply.github.com>
2023-01-05 12:41:20 -0500

    refactored with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
@@ -54,17 +54,31 @@ export const SpeedDialOne = () => {
 }
 
 PrimeReact.ripple = true;
+
+
+
 function App() {
+  
+  useEffect(() => {
+    bindSounds();
+  })
 
-  const handleClick = (event) => {
-    playSound(event.target.value)
+  const bindSounds = () => {
+    let allButtons = document.querySelectorAll(".button");
+    let buttonArray = Array.prototype.slice.call(allButtons)
+    for (let i=0; i<buttonArray.length; i++) {
+      buttonArray[i].addEventListener(('click'), ()=>{
+        let str = buttonArray[i].id + 'audio';
+        testPlaySound(str)
+      })
+    }
   }
 
-  const playSound = (id) => {
-    const e = document.getElementById(id)
-    e.currentTime = 0
-    e.play()
+  const testPlaySound = (id) => {
+    const audio = document.getElementById(id);
+    audio !== null && audio.play();
   }
+
 
   const buttonBank = [
     {
@@ -72,83 +86,100 @@ function App() {
       icon: 'pi pi-heart',
       label: '',
       value: 'button4',
-      sound: switch1
+      sound: switch1,
+      preload: 'auto'
     },
     {
       class: 'p-button-rounded p-button-danger',
       icon: 'pi pi-check',
       label: '',
       value: 'button4',
-      sound: switch2
+      sound: switch2,
+      preload: 'auto'
     },
     {
       class: "p-button-rounded p-button-primary p-button-icon-only",
       icon: 'pi pi-lock-open',
       label: '',
       value: 'button5',
-      sound: switch3
+      sound: switch3,
+      preload: 'auto'
     },
     {
       class: "p-button-rounded p-button-secondary p-button-icon-only",
       icon: 'pi pi-language',
       label: '',
       value: 'button6',
-      sound: click2
+      sound: click2,
+      preload: 'auto'
     },
     {
       class: "p-button-rounded p-button-success p-button-icon-only",
       icon: 'pi pi-globe',
       label: '',
       value: 'button7',
-      sound: slider1
+      sound: slider1,
+      preload: 'auto'
     },
     {
       class: "p-button-rounded p-button-info p-button-icon-only",
       icon: 'pi pi-info-circle',
       label: '',
       value: 'button8',
-      sound: slider2
+      sound: slider2,
+      preload: 'auto'
     },
     {
       class: "p-button-rounded p-button-warning p-button-icon-only",
       icon: 'pi pi-money-bill',
       label: '',
       value: 'button8',
-      sound: selector1
+      sound: selector1,
+      preload: 'auto'
     },
     {
       class: 'p-button-rounded p-button-help',
       icon: 'pi pi-heart',
       label: '',
       value: 'button4',
-      sound: selector2
+      sound: selector2,
+      preload: 'auto'
     }
   ]
 
   const buttons = buttonBank.map(e=>{
     return(
-    <div className="button" >
-      <Button onClick={handleClick} value={e.value} icon={e.icon} label={e.label} className={e.class} />
-      <audio id={e.value} src={e.sound} preload="true" />
+    <div className="button" id={e.value}>
+      <Button icon={e.icon} label={e.label} className={e.class} />
+      <audio id={e.value+"audio"} src={e.sound} preload="auto"></audio>
     </div>
     )
   })
 
   return (
+    
+
     <div className="App">
       <h1>enjoy interacting with these elements...</h1>
-      <div className="button"><Button onClick={handleClick} label='click me!' value="buttonOne"/></div>
-      <audio id="buttonOne" src={selector2} preload="auto" ></audio>
-      <div className="button"><Button onClick={handleClick} icon='pi pi-check' className="p-button-rounded" value="audio2" /></div>
-      <audio id="audio2" src={click1}  ></audio>
-      <div className="button"><Button onClick={handleClick} icon='pi pi-cog' className="p-button-rounded p-button-text p-button-icon-only" value="audio3" /></div>
-      <audio id="audio3" src={power}></audio>
+      
+      <div className="button" id="button1" ><Button label='click me!'/></div>
+      <audio src={selector2} id="button1audio" preload="auto"></audio>
+
+      <div className="button" id="button2"><Button icon='pi pi-check' className="p-button-rounded" value={click1} /></div>
+      <audio src={click1} id="button2audio" preload="auto" ></audio>
+
+      <div id="button3" className="button"><Button icon='pi pi-cog' className="p-button-rounded p-button-text p-button-icon-only" value={power} /></div>
+      <audio id="button3audio" src={power} preload="auto" ></audio>
+      
       <div><SpeedDialOne /></div>
-      <div className="buttonBucket">
-          {buttons}
-      </div>
+      
+      <div className="buttonBucket">{buttons}</div>
+    
     </div>
   );
 }
+
+
+
 
 export default App;
